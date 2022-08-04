fb7102b765e4aea779fa388a66c76ecfae4c62d0
Harshal <hartalkaryash@gmail.com>
2022-08-04 15:33:55 +0530

    commit

---
+++ lib/skippedscreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Sscreen extends StatefulWidget {
+  const Sscreen({Key? key}) : super(key: key);
+
+  @override
+  State<Sscreen> createState() => _SscreenState();
+}
+
+class _SscreenState extends State<Sscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
